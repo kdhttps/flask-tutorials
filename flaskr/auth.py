80727f183297f6e36aff5b755a6268b7b09fa06b
kdhttps <kdhttps@gmail.com>
2020-10-08 19:43:44 +0530

    fix: small changes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -75,7 +75,7 @@ def load_logged_in_user():
     else:
         g.user = get_db().execute(
             'SELECT * FROM user WHERE id = ?', (user_id, )
-        ).fetchone();
+        ).fetchone()
 
 @bp.route('/logout')
 def logout():
